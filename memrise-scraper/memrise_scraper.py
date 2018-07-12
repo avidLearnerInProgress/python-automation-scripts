3273e4116e57eb7184a5b94729ad29bc9d65722e
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-07-12 18:29:52 +0530

    Added memrise scraper

--- memrise-scraper/memrise_scraper.py
+++ memrise-scraper/memrise_scraper.py
@@ -4,12 +4,28 @@ import time
 
 memrise_url = "https://www.memrise.com/course/121215/barrons-800-essential-word-list-gre/"
 sentences_url = "http://sentence.yourdictionary.com/"
-
 mapping = dict()
-combine = []
-glob_s = []
 
-sentence_map = dict()
+def write_to_file(combine, sentence_map):
+
+	with open("memrise_mapper.txt", 'r') as f:
+		contents = f.read()
+
+	with open("memrise_mapper.txt", 'a') as mfile:
+		for ele in combine:
+			if ele[0] in contents:
+				print("Mapping already exists.")
+			else:
+				writetotxt = str(ele[0]) + " => " + str(ele[1]) + "\n"
+				#mfile.write("!-------------------!\n")
+				mfile.write(writetotxt)
+				if ele[0] in sentence_map.keys() and len(sentence_map[ele[0]]) != 0:
+					for s in sentence_map[ele[0]]:
+						mfile.write(s + "\n")
+					mfile.write("!-------------------!\n\n")
+				else:
+					mfile.write("No sentences.. :( \n")
+					mfile.write("!-------------------!\n\n")
 
 def get_sentences(ur):
 	url = ur
@@ -24,42 +40,32 @@ def get_sentences(ur):
 		sentences.append(sentence.text)
 	return sentences
 
-for i in range(1,81):
-	url = memrise_url + str(i) + "/"
-	response = req.urlopen(url)
-	data = response.read()
-	soup = BeautifulSoup(data,'lxml')
-	for w in soup.find_all("div", class_ = "thing text-text"):
-		word = w.find("div", class_ = "col_a col text")
-		meaning = w.find("div", class_ = "col_b col text")
-		sentence = sentences_url + word.text
-		sentences = get_sentences(sentence)
-		sentences = sentences[:4]
-		
-		combine.append((word.text, meaning.text))
-		#glob_s.append(sentences)
-		sentence_map[word.text] = sentences
+if __name__ == '__main__':
+	for i in range(1,81):
+		url = memrise_url + str(i) + "/"
+		response = req.urlopen(url)
+		data = response.read()
+		soup = BeautifulSoup(data,'lxml')
+		combine = []
+		sentence_map = dict()
+		for w in soup.find_all("div", class_ = "thing text-text"):
+			word = w.find("div", class_ = "col_a col text")
+			meaning = w.find("div", class_ = "col_b col text")
+			sentence = sentences_url + word.text
+			time.sleep(1)
+			try:
+				sentences = get_sentences(sentence)
+				sentences = sentences[:3]
+			except Exception:
+				pass
+			combine.append((word.text, meaning.text))
+			sentence_map[word.text] = sentences
 
-		if word.text not in mapping:
-			mapping[word.text] = meaning.text
-			antimapping = dict((y, x) for x,y in mapping.items())
-	time.sleep(3)
-
-with open("memrise_mapper.txt", 'r') as f:
-	contents = f.read()
-
-with open("memrise_mapper.txt", 'a') as mfile:
-	for ele in combine:
-		if ele[0] in contents:
-			print("Mapping already exists.")
-		else:
-			writetotxt = str(ele[0]) + " => " + str(ele[1]) + "\n"
-			#mfile.write("!-------------------!\n")
-			mfile.write(writetotxt)
-			if ele[0] in sentence_map.keys() and len(sentence_map[ele[0]]) != 0:
-				for s in sentence_map[ele[0]]:
-					mfile.write(s + "\n")
-				mfile.write("!-------------------!\n\n")
-			else:
-				mfile.write("No sentences.. :( \n")
-				mfile.write("!-------------------!\n\n")
+			if word.text not in mapping:
+				mapping[word.text] = meaning.text
+				antimapping = dict((y, x) for x,y in mapping.items())
+		write_to_file(combine, sentence_map)
+		sentence_map.clear()
+		combine.clear()
+		time.sleep(5)
+		#break
